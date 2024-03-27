3a5b4fd4a2aefae058ced57ee65ce36ead11f6e0
salvatore sorrentino <salvatore.sorrentino@live.com>
2024-03-27 09:43:05 +0100

    zoom

---
+++ DemoBlazor.Models/MapSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoBlazor.Models;
+
+public class MapSettings
+{
+    public string Id { get; set; } = "map";
+    public double Latitudine { get; set; }
+    public double Longitudine { get; set; }
+    public int Zoom { get; set; }
+}
---
+++ DemoBlazor.WebAssembbly/wwwroot/js/map.js
@@ -0,0 +1,15 @@
+﻿window.showMap = (mapSettings) => {
+    console.log(mapSettings);
+    var map = L.map(mapSettings.id).setView([mapSettings.latitudine, mapSettings.longitudine], mapSettings.zoom);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    var marker = L.marker([mapSettings.latitudine, mapSettings.longitudine]).addTo(map);
+    var circle = L.circle([51.508, -0.11], {
+        color: 'green',
+        fillColor: 'green',
+        fillOpacity: 0.5,
+        radius: 100
+    }).addTo(map);
+};
